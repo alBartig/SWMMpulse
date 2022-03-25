cc150cfc6de4a4f7635af09619250aa322da1142
albert <albert.koenig@tugraz.at>
2022-03-25 11:26:10 +0100

    finished up logics, still got to implement logger

--- sampler.py
+++ sampler.py
@@ -1,141 +1,195 @@
-import numpy as np
 import pandas as pd
 import datetime as dt
-#from prouter import _Route_table, Postprocessing, preparation
-#from pconstants import Loading
+import numpy as np
+
+strategies = {"A":{"kind":"time",
+                   "samplingfreq":f"{1}H",
+                   "samplingduration":60,
+                   "volume":250},
+              "B":{"kind":"time",
+                   "samplingfreq":f"{1/3}H",
+                   "samplingduration":60,
+                   "volume":250},
+              "C":{"kind":"flow",
+                   "samplingfreq":f"{1}H",
+                   "samplingduration":60,
+                   "volume":200},
+              "D":{"kind":"flow",
+                   "samplingfreq":f"{1/3}H",
+                   "samplingduration":60,
+                   "volume":200},
+              "E":{"kind":"volume",
+                   "samplecount":24,
+                   "samplingduration":60,
+                   "volume":50},
+              "F":{"kind":"volume",
+                   "samplecount":72,
+                   "samplingduration":60,
+                   "volume":250},
+              "G":{"kind":"grab",
+                   "samplingtime":dt.time(hour=9),
+                   "samplingduration":120,
+                   "volume":1000},
+              "H":{"kind":"grab",
+                   "samplingtime":dt.time(hour=12),
+                   "samplingduration":120,
+                   "volume":1000}}
+
+class STRATEGY:
+    SAMPLINGTIME = "samplingtime"
+    SAMPLINGFREQ = "samplingfreq"
+    SAMPLINGDURATION = "samplingduration"
+    SAMPLINGVOLUME = "samplingvolume"
+    SAMPLECOUNT = "samplecount"
+    WEIGHTING = "weighting"
+    WEIGHTS = "weights"
+    GRAB = "grab"
+    COMPOSITE = "composite"
 
 class Sampler:
-    def __init__(self, duration=30, frequency="H", volume=0.25, resolution=10):
+    def __init__(self, strategies=strategies):
+        self.strategies = strategies
 
-        #temporary start dates
-        today = dt.datetime.today()
-        start = dt.datetime.combine(today, dt.time(hour=0,minute=0,second=0))
-        end = dt.datetime.combine(today, dt.time(hour=23,minute=59,second=59))
+    def add_strategies(self, strategies):
+        """
+        Adds strategies to the sampling strategy dict.
+        "strategies" is a dictionary containing the strategies and
+        the names as key value pairs:
+        {key1: strategy1, key2: strategy2}
 
-        self.settings = {'duration':duration, 'frequency':frequency, 'volume':volume, 'resolution':resolution}
-        #temporary start times, date needs to be stripped later and replaced with date in given datetimeindex
-        self.sample_times = pd.date_range(start, end, freq=frequency)
+        strategies must contain:
+            "kind":             "time"/"flow"/"volume"/"grab"
+            "samplingtime":     dt.time, time the grab sample is taken
+            "samplingduration": int, duration in seconds, that the sample is taken over
+            "samplingfreq":     float, frequency in hours, the samples are taken
+            "samplingvolume":   In volume weighted sampling, the volume of flow delimiting each sample
+            "flow":             pd.Series, flow
 
-    @property
-    def duration(self):
-        return self.settings['duration']
+        Args:
+            strategies (dict): dictionary of dictionaries containing the strategies
 
-    @property
-    def frequency(self):
-        return self.settings['frequency']
+        Returns: None
 
-    @property
-    def volume(self):
-        return self.settings['volume']
+        """
+        for key, strategy in strategies:
+            self.strategies[key] = strategy
 
-    @property
-    def resolution(self):
-        return self.settings['resolution']
+    def add_flows(self, flow):
+        """
+        Adds flows to the sampler for flow- and volume-weighted sampling
+        Args:
+            flow (pd.Series): Series containing the flows with same index as concentrations
 
-    def sample(self, df):
-        if isinstance(df.columns, pd.MultiIndex):
-            rv = self._sample_mi(df)
-        else:
-            rv = self.extract_series(self._sample_si(df))
-        return rv
+        Returns: None
 
-    def _sample_mi(self, midf):
-        rd = {}
-        i0 = midf.columns[0][:-1]
-        df = self.extract_series(self._sample_si(midf.xs(i0, axis=1)), miprefix=i0)
-        stepsize = len(midf.columns.levels[midf.columns.nlevels-1])
-        for i in midf.columns[stepsize::stepsize]:
-            miprefix = tuple(i[:-1])
-            samples = self._sample_si(midf.xs(miprefix, axis=1))
-            df = df.join(self.extract_series(samples, miprefix=miprefix))
-        return df
-
-    def _sample_si(self, sidf, as_df=True):
-        date = sidf.index[0].date()
-        n = np.floor(self.duration/self.resolution)
-        v = self.volume/n
-        samples = []
-        for time in self.sample_times:
-            t = dt.datetime.combine(date, time.time())
-            slots = pd.date_range(t, periods=n, freq="10S")
-            constituents = dict()
-            for series in sidf:
-                constituents[series] = np.mean([sidf[series][i] for i in slots])
-            samples.append(Sample(time, self.volume, constituents))
-        return samples
+        """
+        self.flows = flow
 
     @staticmethod
-    def extract_series(samples, key="all", miprefix=()):
-        if key == "all":
-            constituents = set.union(*[set(sample.constituents.keys()) for sample in samples])
-        else:
-            constituents = [key]
-        c = {const:[] for const in constituents}
-        t = []
-        for sample in samples:
-            t.append(sample.time)
-            [c[const].append(sample.constituents.get(const,0)) for const in constituents]
-        mi = pd.MultiIndex.from_tuples([miprefix + (const,) for const in constituents])
-        rdf = pd.DataFrame(c, index=t).columns=mi
-        mi = pd.MultiIndex.from_tuples([miprefix + (const,) for const in constituents])
-        rdf = pd.DataFrame(c, index=t)
-        rdf.columns = mi
-        return rdf
+    def get_sampling_hours(freq):
+        start = dt.datetime.today().date()
+        end = pd.to_datetime(dt.datetime.today()).ceil("D")
+        return pd.date_range(start, end, freq=freq, closed="left")
 
+    @staticmethod
+    def get_sampling_times(starttime, duration, interval=10):
+        nsteps = duration/interval
+        interval = f"{interval}S"
+        return pd.date_range(starttime, periods=nsteps, freq=interval)
 
-class Sample:
-    def __init__(self, time, volume, constituents):
-        self.time = time
-        self.volume = volume
-        self.constituents = constituents
+    @staticmethod
+    def sampling_index_time(samplingfreq, duration=120, interval=10):
+        starttimes = Sampler.get_sampling_hours(samplingfreq)
+        sampling_index = []
+        for time in starttimes:
+            sampling_index += Sampler.get_sampling_times(time, duration, interval).to_list()
+        return pd.DatetimeIndex(sampling_index)
 
-    def __repr__(self):
-        print(f"Sample volume: {self.volume}")
-        print(f"Sample time:   {self.time}")
-        print(f"Sample contents:")
-        for key, value in self.constituents.items():
-            print(f"{key}: {value}")
+    def sampling_index_volume(self, samplecount=24, duration=120, interval=10):
+        flows = self.flows
+        date = flows.index[0].date()
+        starttimes = (flows.cumsum()//(flows.sum()/(samplecount-1))).drop_duplicates().index
+        sampling_index = []
+        for time in starttimes:
+            sampling_index += Sampler.get_sampling_times(time, duration, interval).to_list()
+        sampling_index = pd.DatetimeIndex(sampling_index)
+        return sampling_index[sampling_index.date == date]
 
-class Composite(Sample):
-    def __init__(self, samples, weights=None):
-        if weights is None:
-            weights = [1] * len(samples)
-        constituents = {c:[] for c in set.union(*[set(sample.constituents.keys()) for sample in samples])}
-        time = min([sample.time for sample in samples])
-        volume = samples[0].volume
-        for sample in samples:
-            for constituent, concentration in sample.constituents.items():
-                constituents[constituent].append(concentration)
-        for constituent, concentrations in constituents.items():
-            constituents[constituent] = np.sum([c*w for c,w in list(zip(concentrations,weights))])/np.sum(weights)
-        super().__init__(time, volume, constituents)
+    def sample(self, df_timeseries, strategy):
+        """
+        Takes a dataframe with timeseries (index=datetimeindex, columes=measurements) and samples each column
+        according to strategy, returns series with mean sample concentrations
+        Args:
+            df_timeseries (pd.DataFrame): DataFrame of timeseries
+            strategy (dict): dictionary containing {"samplingfreq", "samplingtime", "volume"}
+                            a dict containing sample strategies can be loaded with "from sampler import strategies
 
+        Returns:
+            pd.Series: with mean sample concentrations for each timeseries
 
-def load_pproc(qlut, graph, load=True):
-    print('Test Postprocessing')
-    evalnode = 'MH327-088-1'
-    #routetable = _Route_table.from_file('C:/Users/alber/Documents/swmm/swmmpulse/route_tables/routetable.pickle')
-    routetable = _Route_table.from_file('/mnt/c/Users/albert/Documents/SWMMpulse/route_tables/routetable.pickle')
-    pproc = Postprocessing.from_rtable(routetable, evalnode, qlut, graph)
+        """
+        kind = strategy.get("kind", "time")
+        if kind == "time":
+            samplingindex = Sampler.sampling_index_time(samplingfreq=strategy.get(STRATEGY.SAMPLINGFREQ),
+                                                        duration=strategy.get(STRATEGY.SAMPLINGDURATION))
+            sample_concs = df_timeseries.loc[samplingindex,:].mean()
 
-    #pproc.process_constituent(Loading.FECAL, entry_loc='C:/Users/alber/Documents/swmm/swmmpulse/entries/entries.pickle', load=load)
-    pproc.process_constituent(Loading.FECAL, entry_loc='/mnt/c/Users/albert/Documents/SWMMpulse/entries/entries.pickle', load=load)
-    print("Test postprocessing finished")
-    return pproc
+        elif kind == "flow":
+            samplingindex = Sampler.sampling_index_time(samplingfreq=strategy.get(STRATEGY.SAMPLINGFREQ),
+                                                        duration=strategy.get(STRATEGY.SAMPLINGDURATION))
+            sample_concs = df_timeseries.multiply(self.flows, axis="index")
+            sample_concs = sample_concs.div(self.flows.mean(), axis="index").loc[samplingindex,:].mean()
 
-def test_sampler():
-    qlut, graph = preparation()
-    pproc = load_pproc(qlut, graph, True)
-    sampler = Sampler(30,'H', 0.25)
-    x,y = pproc.Fecal_Matter.timeseries()
-    s = pd.Series(y,index=x,name='Fecal Matter')
-    df = pd.DataFrame(s)
-    samples = sampler.sample(df)
-    print(sampler.settings)
+        elif kind == "volume":
+            samplingindex = self.sampling_index_volume(samplecount=strategy.get(STRATEGY.SAMPLECOUNT),
+                                                        duration=strategy.get(STRATEGY.SAMPLINGDURATION))
+            sample_concs = df_timeseries.loc[samplingindex,:].mean()
 
+        elif kind == "grab":
+            samplingtime = dt.datetime.combine(df_timeseries.index[0].date(), strategy.get(STRATEGY.SAMPLINGTIME))
+            samplingindex = Sampler.get_sampling_times(starttime=samplingtime,
+                                                       duration=strategy.get(STRATEGY.SAMPLINGDURATION))
+            sample_concs = df_timeseries.loc[samplingindex,:].mean()
+
+        sample_concs.rename("concentration", inplace=True)
+        return sample_concs
 
 def main():
-    test_sampler()
+    import matplotlib.pyplot as plt
+    ncols = 100
+
+    # creating test timeseries
+    dtindex = pd.date_range(dt.datetime.today().date(), periods=8640, freq="10S")
+
+    dfdict = {i: np.cumsum(0.005*(np.random.normal(loc=-2+i/(ncols/4), scale=100, size=8640) - 0.5)) + 100\
+              for i in range(ncols)}
+    df_means = pd.DataFrame([(-2+i/(ncols/4)) for i in range(ncols)], columns=["means"])
+    df_timeseries = pd.DataFrame(dfdict, index=dtindex)
+
+    fig, ax = plt.subplots(ncols=2, nrows=3, figsize=[12,12], facecolor="white")
+
+    for k, (name, strategy) in enumerate(strategies.items()):
+        if k < 6:
+            i, j = k//2, k%2
+            samples = Sampler.sample(df_timeseries, strategy)
+            temp = df_means.join(samples)
+
+            temp.plot(x="means",y="concentration", kind="scatter", ax=ax[i, j])
+
+            textstr = '\n'.join([f'freq={float(strategy.get("samplingfreq").strip("H")):.2f} H',
+                                 f'dur={strategy.get("samplingtime")} s',
+                                 f'corr={temp["means"].corr(temp["concentration"]):.2f}'])
+
+            # these are matplotlib.patch.Patch properties
+            props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+
+            # place a text box in upper left in axes coords
+            ax[i, j].text(0.05, 0.95, textstr, transform=ax[i, j].transAxes, fontsize=14,
+                          verticalalignment='top', bbox=props)
+            ax[i, j].set(title=f"strategy {name}", xlabel="fraction of infected")
+    plt.tight_layout()
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
